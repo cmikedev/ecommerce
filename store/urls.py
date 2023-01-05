90dcf34dc0649de9fb066011198107ddc503264f
cmikedev <carberrm@gmail.com>
2023-01-05 16:55:46 +0000

    Add url patterns for html pages in app urls.py

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('contact/', views.contact, name='contact'),
 ]
