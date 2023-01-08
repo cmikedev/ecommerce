fd553423ec4d892fa7500d5d254a4453809ff7cb
cmikedev <carberrm@gmail.com>
2023-01-08 12:58:28 +0000

    Create Product model

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import photos
-
+from .models import photos, Product
 
 admin.site.register(photos)
+admin.site.register(Product)
--- store/models.py
+++ store/models.py
@@ -12,3 +12,14 @@ class Customer(models.Model):
     name = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    manufacturer = models.CharField(max_length=200)
+    image = CloudinaryField('image')
+    description = models.TextField(max_length=1000)
+    price = models.FloatField()
