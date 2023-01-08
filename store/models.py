61066e59116a3916af0ef974a1f157130f1c9a6e
cmikedev <carberrm@gmail.com>
2023-01-08 14:08:45 +0000

    Create ShippingAddress model

--- store/models.py
+++ store/models.py
@@ -42,6 +42,20 @@ class OrderItem(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
 
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    country = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
+
+
 class Contact(models.Model):
     question_type = (
         ("licence", "Licence Type"),
