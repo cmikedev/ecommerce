2d318564c070e89e06825d0a15efb66522a6fa36
cmikedev <carberrm@gmail.com>
2023-01-08 14:12:37 +0000

    Add models to admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import photos, Product, Contact
+from .models import *
 
 admin.site.register(photos)
+admin.site.register(Customer)
 admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
 admin.site.register(Contact)
