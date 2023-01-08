83af38c7081043a23f9f2894a22bbb0abf5a818e
cmikedev <carberrm@gmail.com>
2023-01-08 07:49:12 +0000

    Add Customer class to models.py

--- store/models.py
+++ store/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 
 class photos(models.Model):
     title = models.CharField(max_length=100)
     image = CloudinaryField('image')
+
+
+class Customer(models.Model):
+    name = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200)
