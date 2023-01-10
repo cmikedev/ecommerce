6ddc1e943bb928d61a4d879d01df72ba7d3c3c92
cmikedev <carberrm@gmail.com>
2023-01-10 14:05:34 +0000

    Create processOrder view

--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
-
+import datetime
 from .models import *
 
 
@@ -85,3 +85,33 @@ def checkout(request):
 def contact(request):
     context = {}
     return render(request, 'store/contact.html', context)
+
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
+            
+    else:
+        print('User is not logged in')
+
+    return JsonResponse('Payment submitted. Thank you!', safe=False)
