189d3887840bb8f8e4cfac148b23be2e04499031
cmikedev <carberrm@gmail.com>
2023-01-14 11:48:25 +0000

    Create registration page to register new users

--- members/views.py
+++ members/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
 
 #--------- Code from Django Docs
 
@@ -11,17 +13,33 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            # Redirect to a success page.
             return redirect('store')
         else:
-            messages.success(request, ("There was an error logging in. Please check your details and try again."))
+            messages.success(request, ('There was an error logging in. Please check your details and try again.'))
             return redirect('login')
-    
     else:
         return render(request, 'authenticate/login.html', {})
 
 
 def logout_user(request):
     logout(request)
-    messages.success(request, ("You have been successfully logged out."))
+    messages.success(request, ('You have been successfully logged out.'))
     return redirect('store')
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ('Registration succesful!'))
+            redirect('store')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'authenticate/register_user.html', {'form': form})
+    
