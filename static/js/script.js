fb09d68a8953a01fb48a540d7754d618cb5ff7a2
cmikedev <carberrm@gmail.com>
2023-01-22 13:42:03 +0000

    Format JS according to JSHint

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,27 @@
 /** The below code is taken from Dennis Ivy's tutorial. See Readme -> Credits **/
 
-var updateBtns = document.getElementsByClassName('update-cart')
+var updateBtns = document.getElementsByClassName('update-cart');
 
 for (i = 0; i < updateBtns.length; i++) {
 	updateBtns[i].addEventListener('click', function(){
-		var productId = this.dataset.product
-		var action = this.dataset.action
-		console.log('productId:', productId, 'Action:', action)
-		console.log('USER:', user)
+		var productId = this.dataset.product;
+		var action = this.dataset.action;
+		console.log('productId:', productId, 'Action:', action);
+		console.log('USER:', user);
 
 		if (user == 'AnonymousUser'){
-			console.log('User is not authenticated')
+			console.log('User is not authenticated');
 			
-		}else{
-			updateUserOrder(productId, action)
+		} else {
+			updateUserOrder(productId, action);
 		}
-	})
+	});
 }
 
 function updateUserOrder(productId, action){
-	console.log('User is authenticated, sending data...')
+	console.log('User is authenticated, sending data...');
 
-		var url = '/update_item/'
+		var url = '/update_item/';
 
 		fetch(url, {
 			method:'POST',
@@ -35,6 +35,6 @@ function updateUserOrder(productId, action){
 		   return response.json();
 		})
 		.then((data) => {
-		    location.reload()
+		    location.reload();
 		});
 }
