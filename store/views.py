c5d81b53e74c9e20f7b04ea3c07bbdfd008481fd
cmikedev <carberrm@gmail.com>
2023-01-23 18:21:15 +0000

    Remove redundant code and add modal to add to cart button

--- store/views.py
+++ store/views.py
@@ -77,10 +77,6 @@ def cart(request):
                                                     )
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
-    else:
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
-        cartItems = order['get_cart_items']
 
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
